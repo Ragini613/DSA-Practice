ef2005069992a50fb64eaeb16f9adf6e4f3b4f6d
Ragini Singh <sengarragini15@gmail.com>
2026-06-14 20:24:37 +0530

    Key and Rooms

---
+++ Array/dsa / Keys and Rooms.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        Set<Integer> visited = new HashSet<>();
+        dfs (rooms, 0, visited);
+        return visited.size() == rooms.size();
+    }
+    private void dfs(List<List<Integer>> rooms, int room, Set<Integer> visited){
+        if(visited.contains(room)) return;
+        visited.add(room);
+        for(int key : rooms.get(room)){
+            dfs(rooms, key, visited);
+        }
+    }
+}
