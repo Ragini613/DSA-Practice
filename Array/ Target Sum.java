92089041192854a18fdcaf35dfb0281d80ad8416
Ragini Singh <sengarragini15@gmail.com>
2026-06-26 14:42:49 +0530

    Target sum

---
+++ Array/ Target Sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+       return dfs(nums, 0, 0, target);
+
+    }
+    private int dfs(int[] nums, int index, int sum, int target){
+        if(index == nums.length){
+            return sum == target ? 1 : 0;
+        }
+        int add = dfs(nums, index + 1, sum + nums[index], target);
+        int subtract = dfs(nums, index + 1, sum - nums[index], target);
+        return add + subtract;
+    }
+}
