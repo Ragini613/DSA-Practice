84584d0111f7847ae20120239a371040e1b88b8c
Ragini Singh <sengarragini15@gmail.com>
2026-06-28 22:35:56 +0530

    Detail - Most Stones Removed with Same Row or Column

---
+++ Array/Most Stones Removed with Same Row or Column.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int removeStones(int[][] stones) {
+        int n = stones.length;
+            boolean[] visited = new boolean[n];
+            int components = 0;
+            for(int i = 0; i<n; i++){
+                if(!visited[i]) {
+                    components++;
+                    dfs(stones, visited, i);
+                }
+            }
+            return n - components;
+        }
+        private void dfs(int[][] stones, boolean[] visited, int i){
+            visited[i] = true;
+            for(int j = 0; j< stones.length; j++){
+                if(!visited[j] && (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1])){
+                    dfs(stones, visited, j);
+                }
+            }
+        }
+    }
