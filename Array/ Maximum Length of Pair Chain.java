2e1b85e5358b698e81f6257a8048e5a62a87dbb6
Ragini Singh <sengarragini15@gmail.com>
2026-07-07 19:59:55 +0530

    Maximum Length of Pair Chain

---
+++ Array/ Maximum Length of Pair Chain.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs, (a, b) -> a[1] - b[1]);
+        int ans= 0;
+        int end = Integer.MIN_VALUE;
+        for (int[] pair : pairs){
+            if(pair[0] > end){
+                ans++;
+                end = pair[1];
+            }
+        }
+        return ans;
+    }
+}
